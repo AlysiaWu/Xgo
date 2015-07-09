ed0c7ab869f7ad1de15af24d61df786d8b4cf294
Wenjie <wenjiewuchem@gmail.com>
2015-07-08 22:36:51 -0700

    change script.js

--- client/script.js
+++ client/script.js
@@ -144,8 +144,147 @@ var myApp = angular.module('myApp', ['ngRoute']);
 return factory;
   }); 
 
+
+// ********************************************
  
+ // Guide Profile
+  myApp.controller('GuideProfileController', function($scope, $routeParams, mainfactory){
+    // $scope.review = [];
+    console.log("in the controller");
+    console.log($routeParams.id);
+    mainfactory.getGuide($routeParams.id, function(data){
+      $scope.guide = data;
+      console.log($scope.guide);
+      // for (var x of data.reviews){
+      //   $scope.reviews.push(x);
+      //   console.log(x);
+      // }
+    });
+// addReview method
+
+  })
+
+// Guides Controller 
+     myApp.controller('guidesController', function ($scope,  $location, mainfactory){
+         mainfactory.getGuides(function(data){
+            $scope.guides = data;
+            // console.log($scope.users);
+            });
+          $scope.addGuide = function(){
+              console.log($scope.new_guide);
+              if ($scope.new_guide.password.length<7)
+                {
+                  $scope.err_password = {msg: "the password needs to be at least 7 characters!"};
+                
+                }else{
+                  mainfactory.addGuide($scope.new_guide, function(output){
+                  console.log('checkout');
+                  console.log(output);
+                  $scope.guides.push(output);
+                 $scope.new_guide={};
+                  // $scope.prop2 = "Second";
+                  console.log(output._id);
+                //    $scope.both = sharedProperties.setProperty(output) + $scope.prop2;               
+                $location.path("/guide/" + output._id);
+                  });
+                }
+            }
+              $scope.loginguide= function (){
+          mainfactory.loginguide($scope.login, function(output){
+            // console.log(output[0]._id);
+             $location.path("/guide/" + output[0]._id);
+
+          })
+        }
+      });
+
+// Traveller Controller
+  myApp.controller('travellersController', function ($scope,  $location, mainfactory){
+     mainfactory.getTravellers(function(data){
+        $scope.travellers = data;
+        // console.log($scope.users);
+        });
+
+      $scope.addTraveller = function(){
+          console.log($scope.new_traveller);
+          if ($scope.new_traveller.password.length<7)
+            {
+              $scope.err_password = {msg: "the password needs to be at least 7 characters!"};
+            
+            }else{
+              mainfactory.addTraveller($scope.new_traveller, function(output){
+
+              // console.log($scope.new_traveller);
+              // console.log('checkout');
+              // console.log(output);
+              $scope.travellers.push(output);
+              $scope.new_traveller={};
+              // $scope.prop2 = "Second";
+            //    $scope.both = sharedProperties.setProperty(output) + $scope.prop2;
+              // console.log('transfer');
+              $location.path("/traveller/" + output._id);
+              });
+            }
+        }
+        $scope.logintraveller= function (){
+
+          mainfactory.logintraveller($scope.login, function(output){
+            // console.log(output[0]._id);
+             $location.path("/traveller/" + output[0]._id);
+
+          })
+
+        }
+
+  });
+
+// Traveller Profile
+myApp.controller('TProfileController', function($scope, $routeParams, mainfactory){
+
+    // $scope.review = [];
+    console.log("traveller profile");
+    console.log($routeParams.id);
+    mainfactory.getTraveller($routeParams.id, function(data){
  
+      $scope.traveller = data;
+      $scope.traveller.username;
+    });
+    console.log()
+
+    $scope.fellowTravellers = [];
+    $scope.fellowTravellers = function(){
+      mainfactory.fellowTravellers($scope.traveller.destination, function(output){
+        for (var i = 0; i < output.length; i ++ ){
+          if (output[i].username == $scope.traveller.username){
+            output.splice(i, 1);
+          }
+        }
+        $scope.fellowTravellers = output;
+      })
+    };
+      $scope.searchGuides = function(destination){
+      console.log(destination);
+      mainfactory.searchGuides({area:destination}, function(output){
+        $scope.localGuides = output;
+      });
+
+    }
+
+    $scope.publicMsg = [];
+    mainfactory.socketOn('server_send_message', function(msg){
+      // console.log(msg);
+      $scope.publicMsg.push(msg);
+    });
+
+    $scope.sendMesage = function(){
+      var msg = {
+        message: $scope.userMessage,
+        sender: $scope.traveller.username
+      }
+      mainfactory.socketEmit('client_send_message',msg);
+      $scope.userMessage = "";
+    };
+});
 
 
 
